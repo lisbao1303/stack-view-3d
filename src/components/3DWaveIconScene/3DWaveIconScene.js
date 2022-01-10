f5b948e8939adc78c9faae43c86dd98178cc8226
thiagao0860 <thiagao0860@gmail.com>
2022-01-10 15:57:11 -0300

    3D componets included, first version.

---
+++ src/components/3DWaveIconScene/3DWaveIconScene.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {ThreeJSRender} from './js/MainAmbiente.js'; 
+
+
+class IconScene extends React.Component {
+    constructor(props) {
+        super(props);
+        this.canvasRef = React.createRef();
+    }
+
+    // ******************* COMPONENT LIFECYCLE ******************* //
+    componentDidMount() {
+        // Get canvas, pass to custom class
+        const canvas = this.canvasRef.current;
+        (new ThreeJSRender(canvas)).init();
+
+        // Init any event listeners
+        window.addEventListener('mousemove', this.mouseMove);
+        window.addEventListener('resize', this.handleResize);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // Pass updated props to 
+        const newValue = this.props.whateverProperty;
+        //this.viewGL.updateValue(newValue);
+    }
+
+    componentWillUnmount() {
+        // Remove any event listeners
+        window.removeEventListener('mousemove', this.mouseMove);
+        window.removeEventListener('resize', this.handleResize);
+    }
+
+    // ******************* EVENT LISTENERS ******************* //
+    mouseMove = (event) => {
+        //this.viewGL.onMouseMove();
+    }
+
+    handleResize = () => {
+        //this.viewGL.onWindowResize(window.innerWidth, window.innerHeight);
+    };
+
+    render() {
+        return (
+            <div className="canvasContainer">
+                <canvas ref={this.canvasRef} width={500} height={300}/>
+            </div>
+        );
+    }
+} 
+export default IconScene;
