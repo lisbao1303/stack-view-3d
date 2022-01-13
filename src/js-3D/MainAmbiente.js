fea234e4661ae9498171b7f673bd70f2cfa8bef9
thiagao0860 <thiagao0860@gmail.com>
2022-01-13 05:41:53 -0300

    initiated exported controls

--- src/js-3D/MainAmbiente.js
+++ src/js-3D/MainAmbiente.js
@@ -4,13 +4,25 @@ import * as THREE from 'three/build/three.module.js';
 
 
 class ThreeJSRender{
+    static RENDER_MODE_BEHIND =0;
+    static RENDER_MODE_FIRST =1;
+
     constructor(canvas,creator){
+        //input
         this.r_canvas = canvas;
         this.r_creator = creator;
+        
+        //scene handlers
         this.scene_wrapper = null;
         this.scene_controller = null;
+        
+        //important three js objects
         this.renderer= null;
         this.pointer = null;
+
+        this._onPointerMove = null;
+        this._onResize = null;
+
         if ( WEBGL.isWebGLAvailable()) {
             console.log( 'WebGL is available' );
         } else {
@@ -21,8 +33,8 @@ class ThreeJSRender{
         };
     }
 
-    async init(){
-        //just set the render pass scene    
+    async init(render_mode){
+        //just set the render pass scene
         this.renderer = new THREE.WebGLRenderer({
             canvas: this.r_canvas,
             antialias: true}
@@ -33,20 +45,40 @@ class ThreeJSRender{
         this.renderer.shadowMap.enabled = true;
         this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         this.renderer.shadowMapSoft = true;
-        
-        //used for static rendered shadows
-        //renderer.physicallyCorrectLights = true;
-        
-        [this.scene_wrapper, this.scene_controller] = await this.r_creator(this.r_canvas);
-        this.scene_controller.setRender(this.renderer);
-        this.scene_controller.setControlModel(SceneControler.FIRST_PERSON_CUSTOM_CONTROL,this.render.bind(this));
-        this.scene_controller.player_control.limits = {
-            x: [-300 , 300],
-            y: [74,76],
-            z: [200, 1200]
-        };
-        this.pointer = this.scene_controller.selector.pointer;
-        this.animate(); //Start Animations
+        if (render_mode===ThreeJSRender.RENDER_MODE_BEHIND){    
+            //used for static rendered shadows
+            //renderer.physicallyCorrectLights = true;
+            
+            [this.scene_wrapper, this.scene_controller] = await this.r_creator(this.r_canvas);
+            this.scene_controller.setRender(this.renderer);
+            this.scene_controller.setControlModel(SceneControler.FIRST_PERSON_CUSTOM_CONTROL,this.render.bind(this));
+            this.scene_controller.player_control.limits = {
+                x: [-300 , 300],
+                y: [74,76],
+                z: [200, 1200]
+            };
+            //TODO: limits must come from the scenefactory
+
+            this._onPointerMove = this.onPointerMoveBehind;
+            this._onResize = this.onResizeBehind;
+            this.pointer = this.scene_controller.selector.pointer;
+            this.animate(); //Start Animations
+        }
+        if (render_mode===ThreeJSRender.RENDER_MODE_FIRST){
+            //TODO: scene asignation
+            //this.scene_controller.setControlModel(SceneControler.FIRST_PERSON_CUSTOM_CONTROL,this.render.bind(this));
+            //this.scene_controller.player_control.limits = {
+            //    x: [-300 , 300],
+            //    y: [74,76],
+            //    z: [200, 1200]
+            //};
+
+            this._onPointerMove = this.onPointerMoveBehind;
+            this._onResize = this.onResizeBehind;
+            //TODO: limits must come from the scenefactory
+            this.pointer = this.scene_controller.selector.pointer;
+            this.animate(); //Start Animations
+        }
         
     }
     
@@ -61,20 +93,38 @@ class ThreeJSRender{
     }
 
     onPointerMove( event ) {
-        this.scene_controller.selector.pointer.x = ( event.clientX / this.r_canvas.width ) * 2 - 1;
-        this.scene_controller.selector.pointer.y = - ( event.clientY / this.r_canvas.height ) * 2 + 1;
-        this.scene_controller.player_control.onMouseMove.bind( this.scene_controller.player_control, this.scene_controller.selector.pointer.x,this.scene_controller.selector.pointer.y )();
-        //console.log( event.clientX,  event.clientY );
-        //console.log( this.r_canvas.width, this.r_canvas.height );
-        //console.log(this.scene_controller.selector.pointer);
+        this._onPointerMove( event );
+    }
+    
+    onPointerMoveBehind( event ) {
+            //TODO: (x,y) must be in % values 
+            this.scene_controller.selector.pointer.x = ( event.clientX / this.r_canvas.width ) * 2 - 1;
+            this.scene_controller.selector.pointer.y = - ( event.clientY / this.r_canvas.height ) * 2 + 1;
+            this.scene_controller.player_control.onMouseMove.bind( this.scene_controller.player_control, this.scene_controller.selector.pointer.x,this.scene_controller.selector.pointer.y )();
+            //console.log( event.clientX,  event.clientY );
+            //console.log( this.r_canvas.width, this.r_canvas.height );
+            //console.log(this.scene_controller.selector.pointer);
+    }
+
+    onPointerMoveFirst( event ) {
+        //TODO:
     }
 
     onResize() {
+        this._onResize();
+    }
+
+    onResizeBehind() {
         this.scene_wrapper.cameras[this.scene_wrapper.active_camera].aspect = window.innerWidth/ window.innerHeight;
         this.scene_wrapper.cameras[this.scene_wrapper.active_camera].updateProjectionMatrix();
         this.renderer.setSize( window.innerWidth, window.innerHeight);
         this.scene_controller.player_control.handleResize();
         //controller.player_control.update();
     }
+
+    onResizeFirst() {
+        //TODO:
+    }
+
 }
 export {ThreeJSRender};
