27a821d4cb4f7075b0148e4c4e9ac6a89dceccd4
Gabriel Elisbão <51028131+lisbao1303@users.noreply.github.com>
2022-03-08 19:59:38 -0300

    sw

--- workbox-config.js
+++ workbox-config.js
@@ -11,6 +11,6 @@
       "**/*.{json,ico,png,html,js,css}"
     ],
     "swDest": "build/sw.js",
-    "swSrc": "src/sw.js",
+   // "swSrc": "src/sw.js",
     "injectionPointRegexp": /(const precacheManifest = )\[\](;)/
   };
